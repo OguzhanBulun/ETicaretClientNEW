ced2c060d1781f7dd28fd2cddcf47ff7fcde7325
OguzhanBulun <oguzhanbulun24@gmail.com>
2025-03-01 02:32:37 +0300

    material ui added to project

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule
+    ComponentsModule,
+    MatSidenavModule,
+    RouterModule
   ],
   exports: [
     LayoutComponent
